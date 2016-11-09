9f8af85c02863c3bf701b0d6b4d7d9712275b784
guohc <guohc@primeton.com>
2016-11-09 09:46:13 +0800

    update to support amd specs

--- src/jquery-kanban.js
+++ src/jquery-kanban.js
@@ -1,22 +1,42 @@
-;(function($, window, document, undefined) {
+(function(factory) {
+  if (typeof define === "function" && define.amd) {
 
-  var Kanban = function() {
+    // AMD. Register as an anonymous module.
+    define(["jquery", 'jquery-ui'], factory);
+  } else {
 
+    // Browser globals
+    factory(jQuery);
   }
+}(function($) {
 
-  Kanban.prototype = {
-    createColumn: function() {
-      return ""
-    }
+  var Kanban = function(element, options) {
+    this.$element = $(element);
+    this.$element.addClass("ui-kanban");
+    this.options = options;
+    this.newColumnId = null;
+    this.cardContainers = {};
+    this._init();
+    this._bindEvents();
   };
 
-  var WorkItem = function(options) {
-    //this.create();
-    this.create = function(options) {
+  Kanban.prototype = {
+
+    _createColumn: function(column) {
       return $(
-        "<li>" +
-        "<div class='task' id='" + options.id + "'>" +
-        "<div>" + options.name + "<a class='task-open'>Open</a></div>" +
+        "<div class='column'>" +
+        "<div class='content'>" +
+        "<h3>" + column.name + "</h3>" +
+        "<ul class='cards ui-sortable' id='" + column.id + "'></ul>" +
+        "</div>" +
+        "</div>");
+    },
+
+    _createCard: function(card) {
+      return $(
+        "<li id='" + card.id + "'>" +
+        "<div class='card'>" +
+        "<div>" + card.name + "<a class='card-open'>Open</a></div>" +
         "<button class='ex'>Click to expand</button>" +
         "<div class='btnGroup'>" +
         "<button>ddddddddddd</button>" +
@@ -24,138 +44,198 @@
         "</div>" +
         "</li>"
       )
-    }
-  }
+    },
 
-  //WorkItem.prototype = {
-  //  create: function(options) {
-  //    return $(
-  //      "<li>" +
-  //      "<div class='task' id='" + options.id + "'>" +
-  //      "<div>" + options.name + "<a class='task-open'>Open</a></div>" +
-  //      "<button class='ex'>Click to expand</button>" +
-  //      "<div class='btnGroup'>" +
-  //      "<button>ddddddddddd</button>" +
-  //      "</div>" +
-  //      "</div>" +
-  //      "</li>"
-  //    )
-  //  }
-  //}
+    _init: function() {
+      this.$element.html("");
+      if (this.options && this.options.columns && this.options.columns.length > 0) { //create initial columns and cards
+        for (var i = 0; i < this.options.columns.length; i++) {
+          var column = this.options.columns[i];
+          var $columnEle = this._createColumn(column);
+          this.$element.append($columnEle);
 
+          var $cardsContainer = $columnEle.find("ul");
+          this.cardContainers[column.id] = $cardsContainer;
+          if (column.cards && column.cards.length > 0) {
+            for (var j = 0; j < column.cards.length; j++) {
+              var card = column.cards[j];
+              $cardsContainer.append(this._createCard(card));
+            }
+          }
 
-  $.fn.kanban = function(options) {
-    this.addClass("ui-kanban");
-    var workItem = new WorkItem();
-    if (options) {
-      var _this = this;
-      this.appendWorkItem = function(item) {
-        var $column = $(".ui-kanban #" + item.columnId);
-        if ($column.find(".item-create").length > 0) {
-          $column.find(".item-create").before(workItem.create(item));
-        } else {
-          $(".ui-kanban #" + item.columnId).append(workItem.create(item));
-        }
-      };
-
-      this.removeWorkItem = function(item) {
-        var $item = $(".ui-kanban #" + item.columnId + " #" + item.id).parent();
-        if ($item) {
-          if (options && options.removeItemFn) {
-            options.removeItemFn(item.id, function() {
-              $item.remove();
-            })
-          } else {
-            $item.remove();
+          if (column.hasNew) {
+            this.newColumnId = column.id;
+            $cardsContainer.append($("<div class='card-create'><span>+</span></div>"))
+            $cardsContainer.append($("<div class='card-new'><button id='card-add'>创建</button><button id='card-cancel'>取消</button></div>"));
           }
         }
-      };
 
-      this.updateWorkItem = function(item) {
-        var $item = $(".ui-kanban #" + item.id);
-        if ($item != null) {
-          $(".ui-kanban #" + item.columnId).append($item);
-        }
-      };
-
-      var newColumnId = null;
-      for (var i = 0; i < options.columns.length; i++) {
-        var column = options.columns[i];
-        //var lis = "";
-        //for (var j = 0; j < column.items.length; j++) {
-        //  var item = column.items[j];
-        //  lis += "<li id='" + item.id + "'><div class='task'></div></li>";
-        //}
-
-        //var columnEle = $("<div class='column'><h3>" + column.name + "</h3><ul class='tasks ui-sortable' id='" + column.id + "'>" + lis + "</ul></div>");
-        var columnEle = $("<div class='column'><div class='content'><h3>" + column.name + "</h3><ul class='tasks ui-sortable' id='" + column.id + "'></ul></div></div>");
-        this.append(columnEle);
-        var $column = $("#" + column.id);
-        for (var j = 0; j < column.items.length; j++) {
-          var item = column.items[j];
-          $column.append(workItem.create(item));
-        }
-        if (column.hasNew) {
-          newColumnId = column.id;
-          $column.append($("<div class='item-create'><span>+</span></div>"))
-          $column.append($("<div class='item-new'><button id='item-add'>创建</button><button id='item-cancel'>取消</button></div>"));
-        }
+        $(".ui-kanban .ui-sortable").sortable({
+          connectWith: [".ui-kanban .ui-sortable"], placeholder: "drop-area", items: 'li:not(.ui-state-disabled)'
+        });
       }
 
-      if (newColumnId) {
-        $("#" + newColumnId + " .item-create").on("click", function() {
-          $("#" + newColumnId + " .item-new").show(500);
+    },
+
+    _bindMethod: function(fn) {
+      /**
+       * Bind prototype method to instance scope (similar to CoffeeScript's fat
+       * arrow)
+       */
+      var that = this;
+      return function() {
+        return fn.apply(that, arguments);
+      };
+    },
+
+    _bindEvents: function() {
+
+      $(".ui-kanban .ui-sortable").sortable({
+        connectWith: [".ui-kanban .ui-sortable"], placeholder: "drop-area", items: 'li:not(.ui-state-disabled)',cancel:".card-open"
+      });
+
+      this._sortReceive = this._bindMethod(this._sortReceive);
+      $(".ui-kanban .ui-sortable").on("sortreceive", this._sortReceive);
+
+      this._openCard = this._bindMethod(this._openCard);
+      $(".ui-kanban").on("click", ".ui-sortable .card .card-open", this._openCard);
+
+      $(".ui-kanban").on("click", ".ui-sortable .card .ex", function(e) {
+        $(this).parent().find(".btnGroup").toggle(500);
+        return false;
+      })
+      var _this = this;
+      if (_this.newColumnId) {
+        $("#" + _this.newColumnId + " .card-create").on("click", function() {
+          $("#" + _this.newColumnId + " .card-new").show(500);
           $(this).hide();
           return false;
         })
 
-        $("#" + newColumnId + " .item-new #item-add").on("click", function() {
 
-          if (options && options.itemAddFn) {
-            options.itemAddFn({
+
+        $("#" + _this.newColumnId + " .card-new #card-add").on("click", function() {
+          if (typeof _this.options.cardAddFn == "function") {
+            _this.options.cardAddFn({
               columnId: "first",
               id: Math.random(),
               name: Math.random()
-            }, function(item) {
-              _this.appendWorkItem(item)
-              $("#" + newColumnId + " .item-new").hide();
-              $("#" + newColumnId + " .item-create").show();
+            }, function(card) {
+              _this.appendCard(card)
+              $("#" + _this.newColumnId + " .card-new").hide();
+              $("#" + _this.newColumnId + " .card-create").show();
             })
           }
-
-
           return false;
         })
 
-        $("#" + newColumnId + " .item-new #item-cancel").on("click", function() {
-          $("#" + newColumnId + " .item-new").hide();
-          $("#" + newColumnId + " .item-create").show();
+        $("#" + _this.newColumnId + " .card-new #card-cancel").on("click", function() {
+          $("#" + _this.newColumnId + " .card-new").hide();
+          $("#" + _this.newColumnId + " .card-create").show();
           return false;
         })
       }
-      $(".ui-kanban .ui-sortable").sortable({
-        connectWith: [".ui-kanban .ui-sortable"], placeholder: "drop-area", items: 'li:not(.ui-state-disabled)'
-      });
+    },
 
-      $(".ui-kanban .ui-sortable").on("sortreceive", function(event, ui) {
-        if (options && options.updateItemStatus) {
-          options.updateItemStatus($(ui.item).children(".task").attr("id"), $(ui.item).parent().attr("id"))
+    _unbindEvents: function() {
+      $(".ui-kanban .ui-sortable").off("sortreceive", this._sortReceive);
+      $(".ui-kanban .ui-sortable .card .card-open").off("click", this._openCard);
+      if(this.newColumnId) {
+        $("#" + this.newColumnId + " .card-create").off("click");
+        $("#" + this.newColumnId + " .card-new #card-add").off("click");
+        $("#" + this.newColumnId + " .card-new #card-cancel").off("click");
+      }
+    },
+
+    _sortReceive: function(event, ui) {
+      if (typeof this.options.updateCardStatus == "function") {
+        this.options.updateCardStatus($(ui.item).children(".card").attr("id"), $(ui.item).parent().attr("id"))
+      }
+    },
+
+    _openCard: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      if (this.options.cardClickFn) {
+        this.options.cardClickFn($(e.target).parent().parent().parent().attr("id"));
+      }
+    },
+
+    appendCard: function(card) {
+      var $cardContainer = this.cardContainers[card.columnId];
+      if ($cardContainer.find(".card-create").length > 0) {
+        $cardContainer.find(".card-create").before(this._createCard(card));
+      } else {
+        $cardContainer.append(this._createCard(card));
+      }
+    },
+
+    removeCard: function(card) {
+      var $card = this.cardContainers[card.columnId].find("#"+ card.id);
+      if ($card) {
+        if (typeof this.options.cardRemoveFn == "function") {
+          this.options.cardRemoveFn(card.id, function() {
+            $card.remove();
+          })
+        } else {
+          $card.remove();
         }
-      });
+      }
+    },
 
-      $(".ui-kanban .ui-sortable .task .task-open").on("click", function(e) {
-        if (options.itemClickFn) {
-          options.itemClickFn($(e.target).parent().parent().attr("id"));
+    updateCard: function(card) {
+      var $card = $(".ui-kanban #" + card.id);
+      var $cardContainer = this.cardContainers[card.columnId];
+      if ($card.length > 0 && $cardContainer) {
+        if ($cardContainer.find(".card-create").length > 0) {
+          $cardContainer.find(".card-create").before($card);
+        } else {
+          $cardContainer.append($card);
         }
-      })
+      }
+    },
 
-      $(".ui-kanban").on("click", ".ui-sortable .task .ex", function(e) {
-        $(this).parent().find(".btnGroup").toggle(500);
-        return false;
-      })
+    refresh: function(options) {
+      this.options = options;
+      this.newColumnId = null;
+      this.cardContainers = {};
+      this._init();
+    },
+
+    destroy: function() {
+      this._unbindEvents();
+      this.$element.html("");
+      this.cardContainers = {};
+      this.newColumnId = null;
     }
+  }
 
+  $.fn.kanban = function(options) {
 
+    var instance,
+      method,
+      args;
+    if (typeof(options) == 'string') {
+      method = options;
+      args = Array.prototype.slice.call(arguments, 1);
+    }
+    this.each(function() {
+      instance = $(this).data('_kanban');
+      // The plugin call be called with no method on an existing GridList
+      // instance to re-initialize it
+      if (instance && !method) {
+        instance.destroy();
+        instance = null;
+      }
+      if (!instance) {
+        instance = new Kanban(this, options);
+        $(this).data('_kanban', instance);
+      }
+      if (method) {
+        instance[method].apply(instance, args);
+      }
+    });
+    // Maintain jQuery chain
     return this;
   }
-})(jQuery, window, document);
+}));
